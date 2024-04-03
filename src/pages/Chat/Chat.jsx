047eb0902e9a80952438ff8dff79973897a0e4b3
Onyedikachi123 <onyedikachierugo@gmail.com>
2024-04-03 03:43:34 +0100

    finish working on the static functionality for the chat system

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -4,15 +4,15 @@ import ChatWindow from './ChatWindow';
 const Chat = () => {
     const [messages, setMessages] = useState([]);
 
-    const handleSendMessage = (text) => {
-        if (typeof text === 'string' && ['vendor', 'customer'].includes('customer')) {
-            setMessages([...messages, { text, sender: 'customer' }]);
+    const handleSendMessage = (message) => {
+        if (typeof message.text === 'string') {
+            setMessages([...messages, message]);
         }
     };
 
     return (
-        <div className="h-screen flex  bg-gray-100 p-14">
-            <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full ">
+        <div className="h-screen flex bg-gray-100 p-14">
+            <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full">
                 <ChatWindow messages={messages} onSendMessage={handleSendMessage} />
             </div>
         </div>
